0a03715a9f2ee8e32a83c475c9001b83662c5e89
B.yashwanth <36238382+topperbyashwanth@users.noreply.github.com>
2019-01-22 19:21:11 +0530

    Added files in FCS lab

---
+++ yash.py
@@ -0,0 +1,26 @@
+a=int(input("how many number of candies do you want :"));
+av=6;
+i=1;
+while i >5= a:
+    print("candies");   
+    i=i+1;
+    if i >= av:
+           print("sorry ,out of stock"); 
+           break
+          
+           
+           
+       
+
+print("thankyou");
+        
+        
+        
+        
+
+       
+      
+  
+
+ 
+        
